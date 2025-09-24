e0407db829e4261a971957608a631b251b56dd21
RAT <25477005+doctor4t@users.noreply.github.com>
2025-09-24 16:29:28 +0200

    Fix psycho mode

--- src/main/java/dev/doctor4t/trainmurdermystery/cca/PlayerPsychoComponent.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/cca/PlayerPsychoComponent.java
@@ -1,6 +1,7 @@
 package dev.doctor4t.trainmurdermystery.cca;
 
 import dev.doctor4t.trainmurdermystery.TMM;
+import dev.doctor4t.trainmurdermystery.client.animation.Interpolations;
 import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import dev.doctor4t.trainmurdermystery.index.TMMItems;
 import dev.doctor4t.trainmurdermystery.util.ShopEntry;
@@ -19,7 +20,6 @@ public class PlayerPsychoComponent implements AutoSyncedComponent, ServerTicking
     public static final ComponentKey<PlayerPsychoComponent> KEY = ComponentRegistry.getOrCreate(TMM.id("psycho"), PlayerPsychoComponent.class);
 
     private final PlayerEntity player;
-    private ItemStack batStack;
     public int psychoTicks = 0;
     public void setPsychoTicks(int ticks) {
         this.psychoTicks = ticks;
@@ -49,17 +49,17 @@ public class PlayerPsychoComponent implements AutoSyncedComponent, ServerTicking
 
     @Override
     public void serverTick() {
-        this.psychoTicks--;
+        if (this.psychoTicks > 0) {
+            if (--this.psychoTicks == 0) {
+                stopPsycho();
+            }
+        }
         this.sync();
     }
 
     public void stopPsycho() {
         this.psychoTicks = 0;
-        if (this.batStack != null) {
-            int slot = this.player.getInventory().getSlotWithStack(this.batStack);
-            this.player.getInventory().removeStack(slot);
-            this.batStack = null;
-        }
+        this.player.getInventory().remove(itemStack -> itemStack.isOf(TMMItems.BAT), Integer.MAX_VALUE, this.player.playerScreenHandler.getCraftingInput());
     }
 
     public boolean startPsycho() {
@@ -73,17 +73,10 @@ public class PlayerPsychoComponent implements AutoSyncedComponent, ServerTicking
     @Override
     public void writeToNbt(@NotNull NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
         tag.putInt("psychoTicks", this.psychoTicks);
-        if (this.batStack != null) {
-            tag.put("batStack", this.batStack.encode(registryLookup));
-        }
     }
 
     @Override
     public void readFromNbt(@NotNull NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
         this.psychoTicks = tag.contains("psychoTicks") ? tag.getInt("psychoTicks") : 0;
-        if (tag.contains("batStack")) {
-            ItemStack.fromNbt(registryLookup, tag.get("batStack"))
-                    .ifPresent(itemStack -> this.batStack = itemStack);
-        }
     }
 }
