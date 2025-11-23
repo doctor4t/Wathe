ea2d3e17955d2751ebde8b75041beb9061fb94bf
Omri H <omri.hermon@gmail.com>
2025-11-23 20:17:13 +0200

    disable message commands and info broadcasts (join and death messages)

---
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/MeCommandMixin.java
@@ -0,0 +1,17 @@
+package dev.doctor4t.trainmurdermystery.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.MeCommand;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MeCommand.class)
+public abstract class MeCommandMixin {
+    @Inject(method = "register", at = @At("HEAD"), cancellable = true)
+    private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
---
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/MessageCommandMixin.java
@@ -0,0 +1,17 @@
+package dev.doctor4t.trainmurdermystery.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.MessageCommand;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MessageCommand.class)
+public abstract class MessageCommandMixin {
+    @Inject(method = "register", at = @At("HEAD"), cancellable = true)
+    private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
