c6e4df5a677aab19cd19c9f849dd9565ad0abd2f
Animetrical <88539234+Animetrical@users.noreply.github.com>
2025-11-25 16:32:41 +0000

    First Trial - Replace @WrapOperation with @Inject for setScreen in all occurances.

--- src/main/java/dev/doctor4t/trainmurdermystery/mixin/client/restrictions/MinecraftClientMixin.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/client/restrictions/MinecraftClientMixin.java
@@ -6,11 +6,14 @@ import dev.doctor4t.trainmurdermystery.client.TMMClient;
 import dev.doctor4t.trainmurdermystery.client.gui.screen.ingame.LimitedInventoryScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
@@ -18,12 +21,23 @@ public abstract class MinecraftClientMixin {
     @Nullable
     public ClientPlayerEntity player;
 
-    @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
+    /* @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
     private void tmm$replaceInventoryScreenWithLimitedInventoryScreen(MinecraftClient instance, Screen screen, Operation<Void> original) {
         if (TMMClient.gameComponent.getFade() > 0) {
             return;
         }
 
         original.call(instance, TMMClient.isPlayerAliveAndInSurvival() ? new LimitedInventoryScreen(this.player) : screen);
+    }*/
+
+    @Inject(method = "setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("HEAD"), cancellable = true)
+    private void tmm$redirectInventorySetScreen(Screen screen, CallbackInfo info) {
+        if (screen instanceof InventoryScreen && TMMClient.isPlayerAliveAndInSurvival()) {
+            assert this.player != null;
+
+            MinecraftClient client = (MinecraftClient)(Object)this;
+            client.setScreen(new LimitedInventoryScreen(this.player));
+            info.cancel();
+        }
     }
 }
