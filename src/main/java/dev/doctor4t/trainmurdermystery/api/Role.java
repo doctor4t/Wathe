4d4db9e23631cb7757fb6e658ceaf265156e9d22
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-11-27 18:52:27 -0800

    Add Role$Builder

--- src/main/java/dev/doctor4t/trainmurdermystery/api/Role.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/api/Role.java
@@ -1,5 +1,6 @@
 package dev.doctor4t.trainmurdermystery.api;
 
+import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import net.minecraft.util.Identifier;
 
 public final class Role {
@@ -60,5 +61,85 @@ public final class Role {
 
     public boolean canSeeTime() {
         return canSeeTime;
+    }
+
+    public static class Builder {
+        protected int color = -1;
+        protected boolean isInnocent = false;
+        protected boolean canUseKiller = false;
+        protected MoodType moodType = MoodType.NONE;
+        protected int maxSprintTime = -1;
+        protected boolean canSeeTime = false;
+
+        public Builder() {
+        }
+
+        public Builder color(int color) {
+            this.color = color;
+            return this;
+        }
+
+        /**
+         * Populates the builder with the innocents' default values
+         * (innocent, doesn't see time, max 10-second sprint, real mood)
+         * @return The {@linkplain Builder}, for chaining
+         */
+        public Builder innocent() {
+            return this.isInnocent(true)
+                    .canUseKiller(false)
+                    .canSeeTime(false)
+                    .maxSprintTime(GameConstants.getInTicks(0, 10))
+                    .moodType(MoodType.REAL);
+        }
+
+        public Builder isInnocent(boolean innocent) {
+            this.isInnocent = innocent;
+            return this;
+        }
+
+        /**
+         * Populates the builder with the killers' default values
+         * (killer, sees time, infinite sprint, fake mood)
+         * @return The {@linkplain Builder}, for chaining
+         */
+        public Builder killer() {
+            return this.canUseKiller(true)
+                    .isInnocent(false)
+                    .canSeeTime(true)
+                    .infiniteSprint()
+                    .moodType(MoodType.FAKE);
+        }
+
+        public Builder canUseKiller(boolean canUseKiller) {
+            this.canUseKiller = canUseKiller;
+            return this;
+        }
+
+        public Builder moodType(MoodType moodType) {
+            this.moodType = moodType;
+            return this;
+        }
+
+        public Builder infiniteSprint() {
+            return this.maxSprintTime(-1);
+        }
+
+        public Builder maxSprintTime(int maxSprintTime) {
+            this.maxSprintTime = maxSprintTime;
+            return this;
+        }
+
+        public Builder canSeeTime(boolean canSeeTime) {
+            this.canSeeTime = canSeeTime;
+            return this;
+        }
+
+        public Role build(Identifier id) {
+            return new Role(id, this.color, this.isInnocent, this.canUseKiller, this.moodType, this.maxSprintTime, this.canSeeTime);
+        }
+
+        public Role buildAndRegister(Identifier id) {
+            return TMMRoles.registerRole(this.build(id));
+        }
     }
 }
--- src/main/java/dev/doctor4t/trainmurdermystery/api/TMMRoles.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/api/TMMRoles.java
@@ -10,11 +10,26 @@ import java.util.ArrayList;
 public class TMMRoles {
     public static final ArrayList<Role> ROLES = new ArrayList<>();
 
-    public static final Role DISCOVERY_CIVILIAN = registerRole(new Role(TMM.id("discovery_civilian"), 0x36E51B, true, false, Role.MoodType.NONE, -1, true));
-    public static final Role CIVILIAN = registerRole(new Role(TMM.id("civilian"), 0x36E51B, true, false, Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false));
-    public static final Role VIGILANTE = registerRole(new Role(TMM.id("vigilante"), 0x1B8AE5, true, false, Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false));
-    public static final Role KILLER = registerRole(new Role(TMM.id("killer"), 0xC13838, false, true, Role.MoodType.FAKE, -1, true));
-    public static final Role LOOSE_END = registerRole(new Role(TMM.id("loose_end"), 0x9F0000, false, false, Role.MoodType.NONE, -1, false));
+    public static final Role DISCOVERY_CIVILIAN = new Role.Builder()
+            .color(0x36E51B)
+            .isInnocent(true)
+            .canSeeTime(true)
+            .buildAndRegister(TMM.id("discovery_civilian"));
+    public static final Role CIVILIAN = new Role.Builder()
+            .color(0x36E51B)
+            .innocent()
+            .buildAndRegister(TMM.id("civilian"));
+    public static final Role VIGILANTE = new Role.Builder()
+            .color(0x1B8AE5)
+            .innocent()
+            .build(TMM.id("vigilante"));
+    public static final Role KILLER = new Role.Builder()
+            .color(0xC13838)
+            .killer()
+            .buildAndRegister(TMM.id("killer"));
+    public static final Role LOOSE_END = new Role.Builder()
+            .color(0x9F0000)
+            .buildAndRegister(TMM.id("loose_end"));
 
     public static Role registerRole(Role role) {
         ROLES.add(role);
