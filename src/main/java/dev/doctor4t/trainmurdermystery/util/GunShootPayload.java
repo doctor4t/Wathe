5a70857191d9ccb8d295de54528966cdd3ffdb04
optimal-0 <optimal0efficiency@gmail.com>
2025-11-28 20:26:52 -0800

    Gun and Knife Payload Checks

--- src/main/java/dev/doctor4t/trainmurdermystery/util/GunShootPayload.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/util/GunShootPayload.java
@@ -13,6 +13,7 @@ import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
@@ -21,6 +22,8 @@ import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import org.jetbrains.annotations.NotNull;
 
 public record GunShootPayload(int target) implements CustomPayload {
@@ -36,63 +39,70 @@ public record GunShootPayload(int target) implements CustomPayload {
         @Override
         public void receive(@NotNull GunShootPayload payload, ServerPlayNetworking.@NotNull Context context) {
             ServerPlayerEntity player = context.player();
+
             ItemStack mainHandStack = player.getMainHandStack();
-            if (!mainHandStack.isIn(TMMItemTags.GUNS)) return;
 
-            player.getWorld().playSound(null, player.getX(), player.getEyeY(), player.getZ(), TMMSounds.ITEM_REVOLVER_CLICK, SoundCategory.PLAYERS, 0.5f, 1f + player.getRandom().nextFloat() * .1f - .05f);
-
-            // cancel if derringer has been shot
-            Boolean isUsed = mainHandStack.get(TMMDataComponentTypes.USED);
-            if (mainHandStack.isOf(TMMItems.DERRINGER)) {
-                if (isUsed == null) {
-                    isUsed = false;
-                }
-
-                if (isUsed) {
+            double range = 20d;
+            HitResult collision = ProjectileUtil.getCollision(player, entity -> entity instanceof PlayerEntity user && GameFunctions.isPlayerAliveAndSurvival(player), range);
+            if (collision instanceof EntityHitResult entityHitResult) {
+                if (!mainHandStack.isIn(TMMItemTags.GUNS) || player.getItemCooldownManager().isCoolingDown(player.getMainHandStack().getItem()))
                     return;
+
+                player.getWorld().playSound(null, player.getX(), player.getEyeY(), player.getZ(), TMMSounds.ITEM_REVOLVER_CLICK, SoundCategory.PLAYERS, 0.5f, 1f + player.getRandom().nextFloat() * .1f - .05f);
+
+                // cancel if derringer has been shot
+                Boolean isUsed = mainHandStack.get(TMMDataComponentTypes.USED);
+                if (mainHandStack.isOf(TMMItems.DERRINGER)) {
+                    if (isUsed == null) {
+                        isUsed = false;
+                    }
+
+                    if (isUsed) {
+                        return;
+                    }
+
+                    if (!player.isCreative()) mainHandStack.set(TMMDataComponentTypes.USED, true);
                 }
 
-                if (!player.isCreative()) mainHandStack.set(TMMDataComponentTypes.USED, true);
-            }
+                if (player.getServerWorld().getEntityById(payload.target()) instanceof PlayerEntity target && target.distanceTo(player) < range) {
+                    GameWorldComponent game = GameWorldComponent.KEY.get(player.getWorld());
+                    Item revolver = TMMItems.REVOLVER;
 
-            if (player.getServerWorld().getEntityById(payload.target()) instanceof PlayerEntity target && target.distanceTo(player) < 65.0) {
-                GameWorldComponent game = GameWorldComponent.KEY.get(player.getWorld());
-                Item revolver = TMMItems.REVOLVER;
+                    boolean backfire = false;
 
-                boolean backfire = false;
+                    if (game.isInnocent(target) && !player.isCreative() && mainHandStack.isOf(revolver)) {
+                        // backfire: if you kill an innocent you have a chance of shooting yourself instead
+                        if (game.isInnocent(player) && player.getRandom().nextFloat() <= game.getBackfireChance()) {
+                            backfire = true;
+                            GameFunctions.killPlayer(player, true, player, GameConstants.DeathReasons.GUN);
+                        } else {
+                            Scheduler.schedule(() -> {
+                                if (!context.player().getInventory().contains((s) -> s.isIn(TMMItemTags.GUNS))) return;
+                                player.getInventory().remove((s) -> s.isOf(revolver), 1, player.getInventory());
+                                ItemEntity item = player.dropItem(revolver.getDefaultStack(), false, false);
+                                if (item != null) {
+                                    item.setPickupDelay(10);
+                                    item.setThrower(player);
+                                }
+                                ServerPlayNetworking.send(player, new GunDropPayload());
+                                PlayerMoodComponent.KEY.get(player).setMood(0);
+                            }, 4);
+                        }
+                    }
 
-                if (game.isInnocent(target) && !player.isCreative() && mainHandStack.isOf(revolver)) {
-                    // backfire: if you kill an innocent you have a chance of shooting yourself instead
-                    if (game.isInnocent(player) && player.getRandom().nextFloat() <= game.getBackfireChance()) {
-                        backfire = true;
-                        GameFunctions.killPlayer(player, true, player, GameConstants.DeathReasons.GUN);
-                    } else {
-                        Scheduler.schedule(() -> {
-                            if (!context.player().getInventory().contains((s) -> s.isIn(TMMItemTags.GUNS))) return;
-                            player.getInventory().remove((s) -> s.isOf(revolver), 1, player.getInventory());
-                            ItemEntity item = player.dropItem(revolver.getDefaultStack(), false, false);
-                            if (item != null) {
-                                item.setPickupDelay(10);
-                                item.setThrower(player);
-                            }
-                            ServerPlayNetworking.send(player, new GunDropPayload());
-                            PlayerMoodComponent.KEY.get(player).setMood(0);
-                        }, 4);
+                    if (!backfire) {
+                        GameFunctions.killPlayer(target, true, player, GameConstants.DeathReasons.GUN);
                     }
                 }
 
-                if (!backfire) {
-                    GameFunctions.killPlayer(target, true, player, GameConstants.DeathReasons.GUN);
-                }
+                player.getWorld().playSound(null, player.getX(), player.getEyeY(), player.getZ(), TMMSounds.ITEM_REVOLVER_SHOOT, SoundCategory.PLAYERS, 5f, 1f + player.getRandom().nextFloat() * .1f - .05f);
+
+                for (ServerPlayerEntity tracking : PlayerLookup.tracking(player))
+                    ServerPlayNetworking.send(tracking, new ShootMuzzleS2CPayload(player.getUuidAsString()));
+                ServerPlayNetworking.send(player, new ShootMuzzleS2CPayload(player.getUuidAsString()));
+                if (!player.isCreative())
+                    player.getItemCooldownManager().set(mainHandStack.getItem(), GameConstants.ITEM_COOLDOWNS.getOrDefault(mainHandStack.getItem(), 0));
             }
-
-            player.getWorld().playSound(null, player.getX(), player.getEyeY(), player.getZ(), TMMSounds.ITEM_REVOLVER_SHOOT, SoundCategory.PLAYERS, 5f, 1f + player.getRandom().nextFloat() * .1f - .05f);
-
-            for (ServerPlayerEntity tracking : PlayerLookup.tracking(player))
-                ServerPlayNetworking.send(tracking, new ShootMuzzleS2CPayload(player.getUuidAsString()));
-            ServerPlayNetworking.send(player, new ShootMuzzleS2CPayload(player.getUuidAsString()));
-            if (!player.isCreative())
-                player.getItemCooldownManager().set(mainHandStack.getItem(), GameConstants.ITEM_COOLDOWNS.getOrDefault(mainHandStack.getItem(), 0));
         }
     }
 }
